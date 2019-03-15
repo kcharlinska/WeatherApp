30193b52dfc701abd6c4fc6a3b65be123015049a
Karolina Charlinska <karolina.charlinska@gmail.com>
2019-03-15 16:01:37 +0100

    Add function to get API data

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+document.querySelector('.btn--submit').addEventListener('click', function (e) {
+    e.preventDefault();
+    const txt = document.querySelector('.input').value;
+    $.getJSON('https://api.openweathermap.org/data/2.5/forecast?q=' + txt + '&units=metric&APPID=0c3984878742697ffa63809549271e91', (data) => {
+        console.log(data);
+    });
+});
+
 document.querySelector('.btn--menu').addEventListener('click', () => {
     document.querySelector('.input-wrapper').classList.toggle('active');
 })
